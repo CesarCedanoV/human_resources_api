0fb65e7b7e813cb25a8a90b4ce2d7c2d3ddb0d50
CesarCedanoV <CesarCedanoV@gmail.com>
2018-11-10 03:13:36 -0400

    Fixed a type in the db-password

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,3 @@
 module.exports =  {
-    mongoURI:'mongodb://dbadmin:000000aa>@ds157853.mlab.com:57853/human_resources-dev'
+    mongoURI:'mongodb://dbadmin:000000aa@ds157853.mlab.com:57853/human_resources-dev'
 }  
