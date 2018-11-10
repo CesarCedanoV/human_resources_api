d2de49a9d3b6d292ed8c644b1903a33732695866
CesarCedanoV <CesarCedanoV@gmail.com>
2018-11-10 16:27:52 -0400

    Do some refactor to the employeeReoutes

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,35 +5,30 @@ const Employee = mongoose.model('employees');
 module.exports = app => {
 
   app.post('/api/employees/hire',  async (req,res) => {
-    
-    const employee = new Employee({...req.body});
-
-    // Missing some employee validation
-
-    try {
-      const newEmployee = await employee.save();
-      res.send(newEmployee);
-    }catch(err){
-      res.status(400).json(err);
-    } 
-
+    await new Employee({...req.body})
+      .save((err, doc)=> {
+        err ? res.status(400).json(err) : res.send(doc);
+      });
   });
 
   app.get('/api/employees/all', async (req,res) => {
-    try{
-      const employees = await Employee.find();
-      res.send(employees);
-    }catch(err){
-      res.status(500).json(err);
-    }
-
+    await Employee.find( (err, docs) => {
+      err ? res.status(500).json(err) : res.send(docs);
+    });
   });
 
-  app.get('/api/employees/get/:employee_code', async (req,res) => {
-    const { employee_code } = req.params
-    const employee = await Employee.findOne({ employee_code })
-    res.send(employee);
+  app.get('/api/employees/getby/employeecode=:employee_code', async (req,res) => {
+    const { employee_code } = req.params;
+    await Employee.findOne({ employee_code }, (err,doc)=>{
+      err ? res.status(400).json(err) : res.send(doc);
+    });
   });
 
+  app.put('/api/employees/update/:_id', async (req,res) => {
+    const { _id } = req.params;
+    await Employee.findByIdAndUpdate(_id, req.body, {new:true}, (err,doc)=>{
+       err ? res.status(400).send(err) : res.send(doc);
+    });
+  });
 
 }
